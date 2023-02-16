1b46a48db7dfa233cfd872feca924e74fa78a8fc
izijoe <jospaco2001@yahoo.com>
2023-02-16 14:12:05 -0800

    commit main

---
+++ deck.h
@@ -0,0 +1,48 @@
+#ifndef DECK_H
+#define DECK_H
+
+/**
+ * enum kind_e - card kind
+ * @SPADE: spade kind
+ * @HEART: heart kind
+ * @CLUB: club kind
+ * @DIAMOND: diamond kind
+ */
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
+
+/**
+ * struct cards_s - Playing card
+ *
+ * @value: Value of the card
+ * from "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_nade_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	sruct deck_node_s *next;
+} deck_node_t;
+
+void sort_deck(deck_node_t **deck);
+#endif
+
