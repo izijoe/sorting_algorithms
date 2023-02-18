ec0f5a532dde0792cfa59099e9f53abd544f30b5
izijoe <jospaco2001@yahoo.com>
2023-02-18 12:14:45 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -3,28 +3,28 @@
 
 #include <stdlib.h>
 /**
- * struct listint_s - Doubly linked list node
+ * struct listint_t - Doubly linked list node
  *
  * @n: Integer staored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
-typedef struct listint_s
+typedef struct listint_t
 {
 	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_s;
+	struct listint_t *prev;
+	struct listint_t *next;
+} listint_t;
 
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-void print_list(const listint_s *list);
-listint_s *swap_node(listint_s *node, listint_s **list);
-void insertion_sort_list(listint_s **list);
+void print_list(const listint_t *list);
+listint_t *swap_node(listint_t *node, listint_t **list);
+void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_s **list);
+void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
